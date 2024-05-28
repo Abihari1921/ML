6561adfeed425a88da286cfd466547c119352612
Abihari1921 <abi.instagram.2005@gmail.com>
2024-05-28 23:09:50 +0530

    Update eefhr.py

--- eefhr.py
+++ eefhr.py
@@ -9,10 +9,8 @@ from sklearn.datasets import load_iris
 
 # Load dataset
 dataset = load_iris()
-X = pd.DataFrame(dataset.data)
-X.columns = ['Sepal_Length', 'Sepal_Width', 'Petal_Length', 'Petal_Width']
-y = pd.DataFrame(dataset.target)
-y.columns = ['Targets']
+X = pd.DataFrame(dataset.data, columns=dataset.feature_names)
+y = pd.DataFrame(dataset.target, columns=['Targets'])
 
 # Set up the layout of the Streamlit app
 st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -24,25 +22,23 @@ fig, ax = plt.subplots(1, 3, figsize=(18, 5))
 colormap = np.array(['red', 'lime', 'black'])
 
 # Real Plot
-ax[0].scatter(X.Petal_Length, X.Petal_Width, c=colormap[y.Targets], s=40)
+ax[0].scatter(X['petal length (cm)'], X['petal width (cm)'], c=colormap[y.Targets], s=40)
 ax[0].set_title('Real')
 
 # KMeans Plot
-model = KMeans(n_clusters=3)
+model = KMeans(n_clusters=3, random_state=42)
 model.fit(X)
 predY = np.choose(model.labels_, [0, 1, 2]).astype(np.int64)
-ax[1].scatter(X.Petal_Length, X.Petal_Width, c=colormap[predY], s=40)
+ax[1].scatter(X['petal length (cm)'], X['petal width (cm)'], c=colormap[predY], s=40)
 ax[1].set_title('KMeans')
 
 # GMM Plot
 scaler = preprocessing.StandardScaler()
-scaler.fit(X)
-xsa = scaler.transform(X)
-xs = pd.DataFrame(xsa, columns=X.columns)
-gmm = GaussianMixture(n_components=3)
-gmm.fit(xs)
-y_cluster_gmm = gmm.predict(xs)
-ax[2].scatter(X.Petal_Length, X.Petal_Width, c=colormap[y_cluster_gmm], s=40)
+xsa = scaler.fit_transform(X)
+gmm = GaussianMixture(n_components=3, random_state=42)
+gmm.fit(xsa)
+y_cluster_gmm = gmm.predict(xsa)
+ax[2].scatter(X['petal length (cm)'], X['petal width (cm)'], c=colormap[y_cluster_gmm], s=40)
 ax[2].set_title('GMM Classification')
 
 # Display the plots in Streamlit
